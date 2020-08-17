682562009c62d369a868bd7e7e3acffddbd65147
naoto67 <uver.com67@gmail.com>
2020-08-17 21:31:55 +0900

    syuusei

--- webapp/go/src/torb/event.go
+++ webapp/go/src/torb/event.go
@@ -102,13 +102,20 @@ func getEvents(all bool) ([]*Event, error) {
 	eventDict := make(map[int64]*Event)
 	chErr := make(chan error)
 	go func() {
-		events, err := FetchEventsCache()
+		rows, err := db.Query("SELECT * FROM events ORDER BY id ASC")
+		// events, err := FetchEventsCache()
 		if err != nil {
 			chErr <- err
 			return
 		}
+		defer rows.Close()
 
-		for _, event := range events {
+		for rows.Next() {
+			var event Event
+			if err := rows.Scan(&event.ID, &event.Title, &event.PublicFg, &event.ClosedFg, &event.Price); err != nil {
+				chErr <- err
+				return
+			}
 			if !all && !event.PublicFg {
 				continue
 			}
@@ -116,8 +123,8 @@ func getEvents(all bool) ([]*Event, error) {
 			event.Total = 1000
 			event.Remains = 1000
 			event.Sheets = makeEventSheets(event.Price)
-			eventDict[event.ID] = event
-			events = append(events, event)
+			eventDict[event.ID] = &event
+			events = append(events, &event)
 		}
 		chErr <- nil
 	}()
@@ -218,6 +225,9 @@ func FetchEventsCache() ([]*Event, error) {
 	var res []*Event
 	for i, _ := range d {
 		var e Event
+		if d[i] == nil {
+			continue
+		}
 		err = json.Unmarshal(d[i], &e)
 		fmt.Println("FetchEventsCache: event", e)
 		if err != nil {
