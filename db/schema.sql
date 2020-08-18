f91288f3305878ed19f904bde850b52214984944
naoto67 <uver.com67@gmail.com>
2020-08-18 15:15:59 +0900

    修正

--- db/schema.sql
+++ db/schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
     KEY event_id_and_sheet_id_idx (event_id, sheet_id),
-    PRIMARY KEY (`id`, `event_id`)
+    PRIMARY KEY (`event_id`, `id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
